2ab7f7cb08a4d40ef843b6ab1ba61a5eda960c43
zhangji92 <938361820@qq.com>
2016-09-23 08:14:07 +0800

    北京娱乐

---
+++ Amuse/app/src/main/java/com/example/lenovo/amuse/fragment/BaseFragment.java
@@ -0,0 +1,11 @@
+package com.example.lenovo.amuse.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by lenovo on 2016/9/22.
+ *
+ */
+
+public class BaseFragment extends Fragment {
+}
---
+++ Amuse/app/src/main/java/com/example/lenovo/amuse/fragment/LovePlay.java
@@ -0,0 +1,30 @@
+package com.example.lenovo.amuse.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.lenovo.amuse.R;
+
+/**
+ * Created by lenovo on 2016/9/22.
+ * 同城爱玩
+ */
+
+public class LovePlay extends BaseFragment {
+    /**
+     * 每次创建（Fragment） 都会绘制Fragemnt 的View 组件时回调该方法
+     *
+     * @param inflater           加载布局文件
+     * @param container          加载Layout 布局的 父（ViewGroup）
+     * @param savedInstanceState 是否返回父 ViewGroup  false 为不返回
+     * @return
+     */
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.love_play, container, false);
+        return view;
+    }
+}
