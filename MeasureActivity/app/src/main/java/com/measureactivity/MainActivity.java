f6df91ece010e8446ee3d08feda544fc39832b37
unknown <>
2016-10-23 14:23:41 +0800

    测量Dome

---
+++ MeasureActivity/app/src/main/java/com/measureactivity/MainActivity.java
@@ -0,0 +1,27 @@
+package com.measureactivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListAdapter;
+
+/**
+ * 测量
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button bnt = (Button) findViewById(R.id.bnt_intent);
+        bnt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ListViewActivity.class));
+            }
+        });
+    }
+}
